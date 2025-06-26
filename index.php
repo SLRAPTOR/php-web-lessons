f9d16967f86e87687fff23c132ba9d6b000654b7
chamuditha pasindu <chamudithapasindu54@gmail.com>
2025-06-27 01:08:34 +0530

    day4
    Co-Authored-By: SLRAPTOR <63906862+SLRAPTOR@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,89 +1,62 @@
-<?php
-include './actions/dbconnection.php';
 
 
-$query = "SELECT * FROM product_category";
-$result = $conn->query($query);
-?>
+<?php 
+
+include 'Actions/dbconnection.php';
+
+
+$query ="SELECT * FROM product_category";
+
+$result= $conn->query($query);
+
+ ?>
+
+
+
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <div align="center">
-            <?php
-            if (isset($_GET['msg'])) {
-                ?>
-            <p style="color: red"><?php echo $_GET['msg']; ?></p>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+    
+    <div align="center">
+    	<form method="get" action="advnceSearch.php">
+    		<input type="text" name="keyword">
 
-                <?php
-            }
-            ?>
-            <h2>product manager</h2>
-            <form action="actions/saveproduct.php" method="post" enctype="multipart/form-data">
-                <table border="1">
+<select name="selectedCategory">
+    		<option value="">All Categories</option>
 
-                    <tbody>
-                        <tr>
-                            <td>Product name</td>
-                            <td><input type="text" name="product_name" value="" /></td>
-                        </tr>
-                        <tr>
-                            <td>Description</td>
-                            <td><input type="text" name="product_desc" value="" /></td>
-                        </tr>
-                        <tr>
-                            <td>Buy price</td>
-                            <td><input type="text" name="product_buy_price" value="" /></td>
-                        </tr>
-                        <tr>
-                            <td>Sell price</td>
-                            <td><input type="text" name="product_sell_price" value="" /></td>
-                        </tr>
-                        <tr>
-                            <td>qty</td>
-                            <td><input type="text" name="product_qty" value="" /></td>
-                        </tr>
-                        <tr>
-                            <td>product image</td>
-                            <td><input type="file" name="product_image" value=""></td>
-                        </tr>
-                        <tr>
-                            <td>product category</td>
-                            <td><select name="category">
+    		<?php 
 
-                                    <?php
-                                    if ($result->num_rows > 0) {
-                                        while($row = $result->fetch_assoc()) {
-                                            ?>
-                                    <option value="<?php echo $row['id_product_category']?>"><?php echo $row  ['category_name']?></option>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
-                                </select></td>
-                        </tr>
+    		if ($result->num_rows > 0) {
 
-                        <tr>
-                            <td>status</td>
-                            <td><select name="status">
-                                    <option value="1">active</option>
-                                    <option value="0">inactive</option>
-                                </select></td>
-                        </tr>
+    			while ($row = $result->fetch_assoc()) {
+    				?>
 
-                        <tr>
-                            <td></td>
-                            <td><input type="submit" value="save product" /></td>
-                        </tr>
+    				<option value="<?php echo $row['id_product_category'] ?>"><?php echo $row['category_name']; ?></option>
 
 
-                    </tbody>
-                </table>
+    				<?php
 
-            </form>
-        </div>
-    </body>
+
+
+
+    			}
+    		}
+
+
+    		 ?>
+
+</select>
+<input type="submit" value="search">
+    	</form>
+
+    </div>
+<?php 
+
+
+ ?>
+</body>
 </html>
